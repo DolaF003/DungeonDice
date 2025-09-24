da6adb686b2e975a150c7a41aa8b461972995535
DolaF003 <fakeye@bc.edu>
2025-09-24 08:49:28 -0400

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Dola Fakeye on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
